c01d8aec2969b3a5b60397bc1dc8ff79db0980c1
Lee Harding <lee.harding@circuitpeople.com>
2020-02-26 08:52:51 -0800

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimplestLoadBalancer
+{
+    static class Extensions
+    {
+        static readonly Random rand = new Random();
+        public static K Random<K, V>(this IDictionary<K, (byte weight, V)> items)
+        {
+            var n = rand.Next(0, items.Values.Sum(v => v.weight));
+            return items.FirstOrDefault(kv => (n -= kv.Value.weight) < 0).Key;
+        }
+
+        public static void SendVia(this IPEndPoint ep, UdpClient client, byte[] packet, AsyncCallback cb) =>
+            client.BeginSend(packet, packet.Length, ep, cb, null);
+
+        public static IEnumerable<IPAddress> Private(this NetworkInterface[] interfaces) =>
+            interfaces.Where(i => i.OperationalStatus == OperationalStatus.Up)
+                .Where(i => i.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .SelectMany(i => i.GetIPProperties().UnicastAddresses)
+                .Where(a => a.Address.AddressFamily == AddressFamily.InterNetwork)
+                .Where(a => IPNetwork.IsIANAReserved(a.Address))
+                .Select(a => a.Address);
+
+        public const int SIO_UDP_CONNRESET = -1744830452;
+        public static UdpClient Configure(this UdpClient client)
+        {
+            client.DontFragment = true;
+            client.Client.IOControl((IOControlCode)SIO_UDP_CONNRESET, new byte[] { 0, 0, 0, 0 }, null); // don't throw on disconnect
+            return client;
+        }
+    }
+    class Program
+    {
+        static long received = 0L;
+        static long relayed = 0L;
+        static long responded = 0L;
+
+        /// <summary>
+        /// Sessionless UDP Load Balancer sends packets to targets without session affinity.
+        /// </summary>
+        /// <param name="serverPort">Set the port to listen to and forward to backend targets (default 1812)</param>
+        /// <param name="adminPort">Set the port that targets will send watchdog events (default 1111)</param>
+        /// <param name="clientTimeout">Seconds to allow before cleaning-up idle clients (default 30)</param>
+        /// <param name="targetTimeout">Seconds to allow before removing target missing watchdog events (default 30)</param>
+        /// <param name="defaultTargetWeight">Weight to apply to targets when not specified (default 100)</param>
+        /// <param name="unwise">Allows public IP addresses for targets (default is to only allow private IPs)</param>
+        static async Task Main(int serverPort = 1812, int adminPort = 1111, uint clientTimeout = 30, uint targetTimeout = 30, byte defaultTargetWeight = 100, bool unwise = false)
+        {
+            await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: Welcome to the simplest UDP Load Balancer.  Hit Ctrl-C to Stop.");
+
+            var admin_ip = NetworkInterface.GetAllNetworkInterfaces().Private().First();
+            await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: The server port is {serverPort}.");
+            await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: The watchdog endpoint is {admin_ip}:{adminPort}.");
+            await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: Timeouts are: {clientTimeout}s for clients, and {targetTimeout}s  for targets.");
+            await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: {(unwise ? "*WARNING* " : string.Empty)}"
+                + $"Targets with public IPs {(unwise ? "WILL BE" : "will NOT be")} allowed.");
+
+            using var cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, a) =>
+            {
+                Console.Out.WriteLine($"\r{DateTime.Now:s}: Beginning shutdown procedure.");
+                cts.Cancel();
+                a.Cancel = true;
+            };
+
+            // helper to run tasks with cancellation
+            Task run(Func<Task> func, string name)
+            {
+                return Task.Run(async () =>
+                {
+                    var ct = cts.Token;
+                    while (!ct.IsCancellationRequested)
+                    {
+                        try
+                        {
+                            await func();
+                        }
+                        catch (Exception e)
+                        {
+                            await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: *ERROR* Task {name} encountered a problem: {e.Message}");
+                            await Task.Delay(100); // slow fail
+                        }
+                    }
+                    await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: {name} is done.");
+                });
+            }
+
+            var backends = new ConcurrentDictionary<IPEndPoint, (byte weight, DateTime seen)>();
+            var clients = new ConcurrentDictionary<IPEndPoint, (UdpClient client, DateTime seen)>();
+
+            // task to listen on the server port and relay packets to random backends via a client-specific internal port
+            using var server = new UdpClient(serverPort).Configure();
+            async Task relay()
+            {
+                if (server.Available > 0)
+                {
+                    var packet = await server.ReceiveAsync();
+
+                    Interlocked.Increment(ref received);
+
+                    var client = clients.AddOrUpdate(packet.RemoteEndPoint, ep => (new UdpClient().Configure(), DateTime.Now), (ep, c) => (c.client, DateTime.Now));
+                    var backend = backends.Random();
+                    backend?.SendVia(client.client, packet.Buffer, s => Interlocked.Increment(ref relayed));
+                }
+                else await Task.Delay(10);
+            }
+
+            // helper to get replies asyncronously
+            async IAsyncEnumerable<(UdpReceiveResult result, IPEndPoint ep)> replies()
+            {
+                foreach (var c in clients)
+                    if (c.Value.client.Available > 0)
+                        yield return (await c.Value.client.ReceiveAsync(), c.Key);
+            }
+
+            // task to listen for responses from backends and re-send them to the correct external client
+            async Task reply()
+            {
+                var any = false;
+                await foreach (var (result, ep) in replies())
+                {
+                    server.BeginSend(result.Buffer, result.Buffer.Length, ep, s => Interlocked.Increment(ref responded), null);
+                    any = true;
+                }
+                if (!any) await Task.Delay(10);
+            }
+
+            // task to listen for instances asking to add/remove themselves as a target (watch-dog pattern)
+            using var control = new UdpClient(new IPEndPoint(admin_ip, adminPort)).Configure();
+            async Task admin()
+            {
+                if (control.Available > 0)
+                {
+                    var packet = await control.ReceiveAsync();
+                    var payload = new ArraySegment<byte>(packet.Buffer);
+
+                    var header = payload.Slice(0, 2);
+                    var ip = new IPAddress(payload.Slice(2).Slice(0, 4));
+                    var port = BitConverter.ToUInt16(payload.Slice(6).Slice(0, 2));
+                    var weight = payload.Count > 8 ? payload[8] : defaultTargetWeight;
+                    if (weight > 0 && (unwise || IPNetwork.IsIANAReserved(ip)))
+                    {
+                        var ep = new IPEndPoint(ip, port);
+                        switch (BitConverter.ToInt16(header))
+                        {
+                            case 0x1111:
+                                backends.AddOrUpdate(ep, ep => (weight, DateTime.Now), (ep, d) => (weight, DateTime.Now));
+                                await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: Refresh {ep} (weight {weight}).");
+                                break;
+                            case 0x1186: // see AIEE No. 26
+                                backends.Remove(ep, out var seen);
+                                await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: Remove {ep}.");
+                                break;
+                        }
+                    }
+                    else await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: Rejected {ip}:{port} (weight {weight}).");
+                }
+                else await Task.Delay(10);
+            };
+
+            // task to remove backends and clients we haven't heard from in a while
+            async Task purge()
+            {
+                await Task.Delay(100);
+                var remove_backends = backends.Where(kv => kv.Value.seen < DateTime.Now.AddSeconds(-targetTimeout)).Select(kv => kv.Key).ToArray();
+                foreach (var b in remove_backends)
+                {
+                    backends.TryRemove(b, out var seen);
+                    await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: Expired target {b} (last seen {seen:s}).");
+                }
+                var remove_clients = clients.Where(kv => kv.Value.seen < DateTime.Now.AddSeconds(-clientTimeout)).Select(kv => kv.Key).ToArray();
+                foreach (var c in remove_clients)
+                {
+                    clients.TryRemove(c, out var info);
+                    await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: Expired client {c} (last seen {info.seen:s}).");
+                }
+            }
+
+            // task to occassionally write statistics to the console
+            async Task stats()
+            {
+                await Console.Out.WriteAsync($"{new string('\b', 36)}{received}/{relayed}/{responded}, {clients.Count} => {backends.Count}");
+                await Task.Delay(500);
+            }
+
+            var tasks = new[] {
+                run(relay, "Relay"),
+                run(reply, "Reply"),
+                run(admin, "Admin"),
+                run(purge, "Purge"),
+                run(stats, "State")
+            };
+            await Task.WhenAll(tasks);
+            var e = string.Join(", ", tasks.Where(t => t.Exception != null).Select(t => t.Exception.Message));
+            await Console.Out.WriteLineAsync($"\r{DateTime.Now:s}: Bye-now ({(e.Any() ? e : "OK")})");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,97 @@
+![.NET Core](https://github.com/mlhpdx/SimplestLoadBalancer/workflows/.NET%20Core/badge.svg)
+
+# Simplest UDP Load Balancer #
+
+This is **not a production load balancer**, just code that demonstrates sessionless load balancing of UDP traffic. 
+
+## Why? ##
+
+Some simple UDP protocols are stateless and there is no advantage in trying to maintain "affinity" between clients and back-end instances.  Traditional load balancers assume that affinity is helpful, and so they will try to route packets from one client to one server. By contrast, this code demonstrates a load balancer that evenly (randomly) distributes packets over all available back-ends. One advantage of this approach for "simple" UDP is that if one backend instance fails there will be an increase in packet loss for all clients rather than a loss of all traffic for some clients (as traditional load balancers would do).
+
+## Building ##
+
+This is a very simple .Net Core 3.0 project, so to build (assuming you have the SDK installed):
+
+```
+dotnet build
+```
+
+## Usage ##
+Likewise, it's simple to run using `dotnet run` in the project directory:
+
+```
+$ dotnet run
+```
+
+If you'd like to generate a single-file executable, which is convenient just target the platform you'll be running on. For Linux:
+
+```
+dotnet publish -o ./ -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
+```
+
+Or for Windows:
+
+```
+dotnet publish -o ./ -c Release -r win10-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
+```
+
+By default the process will listen on port `1812` for any incomming UDP packets and relay them one to one of the IP addres/port targets it knows.  You can control the port it listens on with the `--server-port` option.  Other options are described in the command help:
+
+```
+SimplestLoadBalancer:
+  Sessionless UDP Load Balancer sends packets to targets without session affinity.
+
+Usage:
+  SimplestLoadBalancer [options]
+
+Options:
+  --server-port <server-port>                        Set the port to listen to and forward to backend targets (default 1812)
+  --admin-port <admin-port>                          Set the port that targets will send watchdog events (default 1111)
+  --client-timeout <client-timeout>                  Seconds to allow before cleaning-up idle clients (default 30)
+  --target-timeout <target-timeout>                  Seconds to allow before removing target missing watchdog events (default 30)
+  --default-target-weight <default-target-weight>    Weight to apply to targets when not specified (default 100)
+  --unwise                                           Allows public IP addresses for targets (default is to only allow private IPs)
+  --version                                          Show version information
+  -?, -h, --help                                     Show help and usage information
+```
+
+To add and maintain targets send periodic UDP packets to the admin port (default 1111) on the first private IPv4 address configured on your machine.  The packet format is very simple, consisting of a couple magic bytes to avoid spurrious adds, four bytes for an ipv4 address and two byes for the port number to target:
+
+```
+0x11 0x11 [four bytes for ip] [two bytes for port] [one byte for weight (optional)]
+```
+
+Each time such a packet is received the backend's "last seen" time is updated. The weight is optional, and if not in the packet the value specified at the command line will be used.  Weights are applied in the traditional manner.  
+
+If 30 seconds passes without a backend being seen, it is removed. To immeadiately remove a target send a packet with 0x86 as the first byte instead of 0x11:
+
+```
+0x86 0x11 [four bytes for ip] [two bytes for port]
+```
+
+Using Linux bash, it's easy to send those packets to `/dev/udp/[admin ip]/[admin port]`. For example, if your load balancer is listening on the default admin port `1111` at `192.168.1.11`, and you want to add a target with the IP `192.168.1.22` and port number `1812`:
+
+```bash
+$ echo -e $(echo  "\x11\x11$(echo "192.168.1.22" | tr "." "\n" | xargs printf '\\x%02X')\x14\x07") > /dev/udp/192.168.1.11/1111
+```
+To use a different port on the target, just change the `\x14\07` to the little endian hex representation of port number of your choice.
+
+It can be tedius to manually send those packets and keep a target registered. A simple way to maintain a set of IPs as targets for testing is to create a small shell script, say `lb.sh`:
+
+```bash
+#!/bin/bash
+echo -e $(echo  "\x11\x11$(echo "192.168.1.22" | tr "." "\n" | xargs printf '\\x%02X')\x14\x07") > /dev/udp/192.168.1.11/1111
+echo -e $(echo  "\x11\x11$(echo "192.168.1.23" | tr "." "\n" | xargs printf '\\x%02X')\x14\x07") > /dev/udp/192.168.1.11/1111
+echo -e $(echo  "\x11\x11$(echo "192.168.1.24" | tr "." "\n" | xargs printf '\\x%02X')\x14\x07") > /dev/udp/192.168.1.11/1111
+echo -e $(echo  "\x11\x11$(echo "192.168.1.25" | tr "." "\n" | xargs printf '\\x%02X')\x14\x07") > /dev/udp/192.168.1.11/1111
+echo -e $(echo  "\x11\x11$(echo "192.168.1.26" | tr "." "\n" | xargs printf '\\x%02X')\x14\x07") > /dev/udp/192.168.1.11/1111
+echo -e $(echo  "\x11\x11$(echo "192.168.1.27" | tr "." "\n" | xargs printf '\\x%02X')\x14\x07") > /dev/udp/192.168.1.11/1111
+```
+
+And then use the `watch` command to call that script every few seconds:
+
+```bash
+$ watch -n10 ./lb.sh
+```
+
+Enjoy!
